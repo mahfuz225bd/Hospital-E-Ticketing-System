41bc874f3eb74b452fa4102e9923efc9481dd2b9
Muhammad Sultan Al Mahfuz <60539168+mahfuz225bd@users.noreply.github.com>
2023-10-29 23:14:16 +0600

    removed .venv

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/api/divisions')
+def divisions():
+    return None
+
 if __name__ == '__main__':
     app.run(debug=True)
